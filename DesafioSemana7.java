b3ab5f6da2534cdd13f9dfdd0e519b61f1421db1
Luiz-Carlos-Alves <100029381+Luiz-Carlos-Alves@users.noreply.github.com>
2022-03-24 22:08:03 -0300

    Add files via upload

---
+++ DesafioSemana7.java
@@ -0,0 +1,34 @@
+
+//Autor: Luiz Carlos Alves
+
+package desafiosemana7;
+
+
+public class DesafioSemana7 {
+
+  
+    public static void main(String[] args) {
+      
+        Empregado emp = new Empregado("Luiz Carlos", "Alves", 1500f);
+        System.out.println("O salário anual de " + emp.getNome()
+        + " é de R$ " + (emp.getsalarioAno())); 
+        
+        Empregado luiza = new Empregado ("Luiza", "Fernandes", 2000f);
+        System.out.println("O salário anual de " + luiza.getNome() 
+        + " é de R$ " + (luiza.getsalarioAno()));
+        
+        System.out.println();
+        
+        emp.acrescentarAumento();
+        luiza.acrescentarAumento();
+        
+        System.out.println();
+        
+        System.out.println("O salário anual de " + emp.getNome()
+        + " com 10% de aumento fica: R$ " +  String.format("%.2f", emp.acrescentarAumento()));                
+        
+        System.out.println("O salário anual de " + luiza.getNome()
+        + " com 10% de aumento fica: R$ " +  String.format("%.2f", luiza.acrescentarAumento()));
+    }
+    
+}
